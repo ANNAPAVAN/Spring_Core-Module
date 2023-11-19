e26623d3df2fd0a1162850d2d4a997d5e1709ea7
ANNAPAVAN <123437111+ANNAPAVAN@users.noreply.github.com>
2023-11-19 14:21:31 +0530

    P-NameSpace
    P-NameSpace in Sprin Framework

---
+++ app20_PNamespace/src/main/java/com/pavan/beans/Account.java
@@ -0,0 +1,35 @@
+package com.pavan.beans;
+
+public class Account {
+	private String accNo;
+	private String accName;
+	private String accType;
+	private String balance;
+	
+	public String getAccNo() {
+		return accNo;
+	}
+	public void setAccNo(String accNo) {
+		this.accNo = accNo;
+	}
+	public String getAccName() {
+		return accName;
+	}
+	public void setAccName(String accName) {
+		this.accName = accName;
+	}
+	public String getAccType() {
+		return accType;
+	}
+	public void setAccType(String accType) {
+		this.accType = accType;
+	}
+	public String getBalance() {
+		return balance;
+	}
+	public void setBalance(String balance) {
+		this.balance = balance;
+	}
+	
+	
+}
